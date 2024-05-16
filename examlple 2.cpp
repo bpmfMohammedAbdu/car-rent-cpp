cc3b6be1f9582a7418a1f2a8419d6aa94ecb962d
bpmfMohammedAbdu <155623175+bpmfMohammedAbdu@users.noreply.github.com>
2024-05-16 14:58:22 +0300

    Add files via upload
    ic
    
    
    
    
    
    
    
    
    
    
     a accepting any number from a user and is divisible 5 or not

---
+++ examlple 2.cpp
@@ -0,0 +1,19 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int x=5;
+    int z; 
+    cout<<"intre the number";
+    cin>>z;
+    if(z%5==0)
+    cout<<"the number z is divisible by five"<<endl;
+    else
+    cout<<"the number z is not divisible by five"<<endl;
+    return 0;
+    
+
+ }
+    
+
+    
+ 
