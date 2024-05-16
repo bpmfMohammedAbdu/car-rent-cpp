72176a4bd7703b736f57f6f952a89cc6b7cb7c0c
bpmfMohammedAbdu <155623175+bpmfMohammedAbdu@users.noreply.github.com>
2024-05-16 14:50:14 +0300

    Add files via upload

---
+++ potenianl energy.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+float m=6.5,h=7.4,g=9.81,p;
+bool b=true;
+string v="ahlu";
+p=m*h*g; 
+cout<<"pe="<<p<<"j"<<endl;
+cout<<"b"<<endl;
+cout<<v<<endl;
+cout<<b<<endl;
+return 0;
+
+}
